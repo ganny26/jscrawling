5a1f23ed9310aca54e478f613f9c455e99320931
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-08 18:14:32 +0530

    crawling url's

---
+++ crawlurl.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "ARCHANA_KITCHEN":"https://www.archanaskitchen.com/sweet-lime-soda-recipe",
+    "TIMESOFINDIA":"http://recipes.timesofindia.com/recipes/ice-cream-cake-with-oreo-cookies/rs53665434.cms",
+    "SANJEEV_KAPOOR":"http://www.sanjeevkapoor.com/Recipe/Eggless-Mawa-Elaichi-Doughnuts-Sanjeev-Kapoor-Kitchen-FoodFood.html"
+}
