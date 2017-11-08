a7f044fbee03ca4639368f95cde872fed9f9cf0a
selvaganeshr <selvaganeshrajam@gmail.com>
2017-11-08 22:51:22 +0530

    added new test url's

--- crawlurl.js
+++ crawlurl.js
@@ -1,5 +1,9 @@
 module.exports = {
     "ARCHANA_KITCHEN":"https://www.archanaskitchen.com/sweet-lime-soda-recipe",
     "TIMESOFINDIA":"http://recipes.timesofindia.com/recipes/ice-cream-cake-with-oreo-cookies/rs53665434.cms",
-    "SANJEEV_KAPOOR":"http://www.sanjeevkapoor.com/Recipe/Eggless-Mawa-Elaichi-Doughnuts-Sanjeev-Kapoor-Kitchen-FoodFood.html"
+    "SANJEEV_KAPOOR":"http://www.sanjeevkapoor.com/Recipe/Eggless-Mawa-Elaichi-Doughnuts-Sanjeev-Kapoor-Kitchen-FoodFood.html",
+    "VAREHVAH":"https://www.vahrehvah.com/honey-fish-recipe",
+    "FOOD_NDTV":"http://food.ndtv.com/recipe-seafood-broth-914361",
+    "SECRET_INDIAN":"http://secretindianrecipe.com/recipe/peas-pulao-recipe",
+    "COOKING_SHOOKING":"http://cookingshooking.com/recipe/attacake-dprc/"
 }
