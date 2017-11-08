f60a07c4e63c9c12c151992455175fb07eba251b
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-08 18:14:17 +0530

    crawl by url added

---
+++ crawlbyurl.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const Xpath = require('./xpath');
+
+
+module.exports = {
+    crawlbyurl:function(crawl_url){
+        return new Promise(function(resolve, reject) {
+            request(crawl_url, function(err, res, body) {
+                var $ = cheerio.load(body);
+                let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
+                let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
+                let methodkeywords = ['directions','directions for','methods','how to'];
+                ingredientTags = Xpath.getElementByXpath(body, tags, ingredientkeywords);
+                methodTags = Xpath.getElementByXpath(body, tags, methodkeywords);
+    
+                var obj = {
+                    "ingredients": ingredientTags,
+                    "method": methodTags
+                }
+                resolve(obj);
+            })
+        })
+    }
+}
+
+
+
+
