cd265f308d58574e929b89d4a510dcf9f1b140f6
selvaganeshr <selvaganeshrajam@gmail.com>
2017-11-08 22:51:32 +0530

    added timeout's

--- test/crawlbyurl.test.js
+++ test/crawlbyurl.test.js
@@ -8,11 +8,11 @@ const request = require('request');
 
 
 describe('Archana Kitchen', function () {
-    beforeEach(function (done) {
-        done();
+    beforeEach(function () {
+        this.timeout(5000);
     });
     it('length check', function () {
-        this.timeout(3000);
+      //  this.timeout(5000);
         var x = "";
         var t = [];
         return crawl.crawlbyurl(crawlurl.ARCHANA_KITCHEN).then((data) => {
@@ -39,8 +39,8 @@ describe('Archana Kitchen', function () {
 
 
 describe('Recipe Timesofindia', function () {
-    beforeEach(function (done) {
-        done();
+    beforeEach(function () {
+        this.timeout(5000);
     });
     it('length check', function () {
         var x = "";
@@ -68,8 +68,8 @@ describe('Recipe Timesofindia', function () {
 });
 
 describe('Sanjeev Kapoor', function () {
-    beforeEach(function (done) {
-        done();
+    beforeEach(function () {
+        this.timeout(5000);
     });
     it('length check', function () {
         var x = "";
@@ -91,6 +91,145 @@ describe('Sanjeev Kapoor', function () {
         return crawl.crawlbyurl(crawlurl.SANJEEV_KAPOOR).then((data) => {
             let methodText = data.method[0].text;
             expect(methodText).to.equal('How to make Eggless Mawa Elaichi Doughnuts -  ');
+        })
+
+    })
+});
+
+describe('Varehvah', function () {
+    // beforeEach(function () {
+    //     this.timeout(9000);
+    // });
+    it('length check', function (done) {
+        var x = "";
+        var t = [];
+        
+        return crawl.crawlbyurl(crawlurl.VAREHVAH).then((data) => {
+            setTimeout(()=>{
+                t.push(data);
+                x = t[0].ingredients;
+                expect(x).to.have.lengthOf(2);
+            },10000);
+          
+           
+        })
+       
+    })
+    it('ingredient tag check', function (done) {
+
+        return crawl.crawlbyurl(crawlurl.VAREHVAH).then((data) => {
+
+            setTimeout(()=>{
+                let ingredientText = data.ingredients[0].text;
+                expect(ingredientText).to.equal('Ingredients for Eggless Mawa Elaichi Doughnuts Recipe');  
+            },10000)
+          
+  
+           })
+          
+
+    })
+    it('method tag check', function (done) {
+        return crawl.crawlbyurl(crawlurl.VAREHVAH).then((data) => {
+            setTimeout(function() {
+                let methodText = data.method[0].text;
+                expect(methodText).to.equal('How to make Eggless Mawa Elaichi Doughnuts -  ');
+            }, 10000);
+           
+           
+            
+        })
+       
+    })
+});
+
+describe('Food Ndtv', function () {
+    beforeEach(function () {
+        this.timeout(5000);
+    });
+    it('length check', function () {
+        var x = "";
+        var t = [];
+        return crawl.crawlbyurl(crawlurl.FOOD_NDTV).then((data) => {
+            t.push(data);
+            x = t[0].ingredients;
+            expect(x).to.have.lengthOf(3);
+        })
+    })
+    it('ingredient tag check', function () {
+        return crawl.crawlbyurl(crawlurl.FOOD_NDTV).then((data) => {
+            let ingredientText = data.ingredients[0].text;
+            expect(ingredientText).to.equal('Ingredients of Seafood Broth');           
+           })
+
+    })
+    it('method tag check', function () {
+        return crawl.crawlbyurl(crawlurl.FOOD_NDTV).then((data) => {
+            let methodText = data.method[0].text;
+            expect(methodText).to.equal('How to Make Seafood Broth');
+        })
+
+    })
+});
+
+
+
+
+describe('Scretindianrecipe.com', function () {
+    beforeEach(function () {
+        this.timeout(5000);
+    });
+    it('length check', function () {
+        var x = "";
+        var t = [];
+        return crawl.crawlbyurl(crawlurl.SECRET_INDIAN).then((data) => {
+            t.push(data);
+            x = t[0].ingredients;
+            expect(x).to.have.lengthOf(1);
+        })
+    })
+    it('ingredient tag check', function () {
+        return crawl.crawlbyurl(crawlurl.SECRET_INDIAN).then((data) => {
+            let ingredientText = data.ingredients[0].text;
+            expect(ingredientText).to.equal('Ingredients:&nbsp;');           
+           })
+
+    })
+    it('method tag check', function () {
+        return crawl.crawlbyurl(crawlurl.SECRET_INDIAN).then((data) => {
+            let methodText = data.method[0].text;
+            expect(methodText).to.equal('How To Use');
+        })
+
+    })
+});
+
+
+
+describe('Cookingshooking.com', function () {
+    beforeEach(function () {
+        this.timeout(5000);
+    });
+    it('length check', function () {
+        var x = "";
+        var t = [];
+        return crawl.crawlbyurl(crawlurl.COOKING_SHOOKING).then((data) => {
+            t.push(data);
+            x = t[0].ingredients;
+            expect(x).to.have.lengthOf(2);
+        })
+    })
+    it('ingredient tag check', function () {
+        return crawl.crawlbyurl(crawlurl.COOKING_SHOOKING).then((data) => {
+            let ingredientText = data.ingredients[0].text;
+            expect(ingredientText).to.equal('Ingredients used in ');           
+           })
+
+    })
+    it('method tag check', function () {
+        return crawl.crawlbyurl(crawlurl.COOKING_SHOOKING).then((data) => {
+            let methodText = data.method[0].text;
+            expect(methodText).to.equal('Instructions');
         })
 
     })
